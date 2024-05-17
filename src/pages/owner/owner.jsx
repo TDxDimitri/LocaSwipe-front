85338744c6b894f3eac0c6058afed8ed5a9fe351
Dimitri <talleu.dimitri@outlook.fr>
2024-05-17 16:08:23 +0200

    newNavbarComponent

--- src/pages/owner/owner.jsx
+++ src/pages/owner/owner.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import NavBar from '../../components/ui/NavBar/navBar';
 
 const OwnerHomePage = () => {
     return (
         <div>
+            < NavBar />
             <h1>Bienvenue propriétaire</h1>
         </div>
     );
--- src/pages/tenant/tenant.jsx
+++ src/pages/tenant/tenant.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import NavBar from '../../components/ui/NavBar/navBar';
 
 const TenantHomePage = () => {
     return (
         <div>
+             < NavBar />
             <h1>Bienvenue locataire</h1>
         </div>
     );
