6b606ba57fb4fba2a13c33e739bb2057ec059778
maxstein <supremetnf54@gmail.com>
2024-06-07 12:26:49 +0200

    login page desgin

--- src/components/ui/ButtonAuth/ButtonAuth.jsx
+++ src/components/ui/ButtonAuth/ButtonAuth.jsx
@@ -4,8 +4,8 @@ import './ButtonAuth.scss'; // Assurez-vous que le chemin est correct
 
 function ButtonAuth({ text, position, onClick }) {
     const borderRadiusStyle = position === "left"
-        ? { borderRadius: "0px 20px 20px 0px" }
-        : { borderRadius: "20px 0px 0px 20px" };
+        ? { borderRadius: "4px" }
+        : { borderRadius: "4px" };
     const containerClass = `button-auth-container ${position}`;
 
     return (
