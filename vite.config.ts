8669267c3cb3aa2814dbd2dd29a8cb3310ccc26f
maxstein <supremetnf54@gmail.com>
2024-06-12 02:04:08 +0200

    add PWA to the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,  // Permet de tester le service worker en mode développement
+      },
+      includeAssets: ['vite.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'LocaSwipe',
+        short_name: 'LocaSwipe',
+        description: 'LocaSwipe App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/logo192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
