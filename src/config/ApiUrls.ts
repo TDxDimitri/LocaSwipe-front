9a372587d4f886198f925608fa112a4c80bcc5bd
maxstein <supremetnf54@gmail.com>
2024-06-24 10:27:45 +0200

    fix base url

--- src/config/ApiUrls.ts
+++ src/config/ApiUrls.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://api.locaswipe.fr:3000/";
 // const LOCAL_URL = "http://localhost:3000";
 // https://api.locaswipe.fr:3000/
 
